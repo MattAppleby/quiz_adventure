5b722fce7d0ca6ce4da0fcd75f936031d8dae5e8
matthew appleby <mappleby18@VenerableBede.schools.sunderland.gov.uk>
2016-11-25 09:58:09 +0000

    My game bro

--- adventure.py
+++ adventure.py
@@ -1,11 +1,474 @@
 # An adventure game
+health = 100
+pride = 10
+from random import randint
+from time import sleep
+def trollface():
+    sleep(1)
+    print("You stand on a lego brick")
+    sleep(3)
+    print("You try to pull it together, but the pain overwhelms you")
+    sleep(5)
+    print("Your heart starts to fail, you collapse during the night due to cardiac arrest")
+    sleep(0.5)
+    print(" ░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄")
+    sleep(0.5)
+    print(" ░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄")
+    sleep(0.5)
+    print(" ░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█")
+    sleep(0.5)
+    print(" ░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░█")
+    sleep(0.5)
+    print(" ░▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░█")
+    sleep(0.5)
+    print(" █▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒█")
+    sleep(0.5)
+    print(" █▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█")
+    sleep(0.5)
+    print(" ░█▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█")
+    sleep(0.5)
+    print(" ░░█░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█")
+    sleep(0.5)
+    print(" ░░░█░░██░░▀█▄▄▄█▄▄█▄████░█")
+    sleep(0.5)
+    print(" ░░░░█░░░▀▀▄░█░░░█░███████░█")
+    sleep(0.5)
+    print(" ░░░░░▀▄░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█")
+    sleep(0.5)
+    print(" ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█")
+    sleep(0.5)
+    print(" ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█")
+    sleep(0.5)
+    print(" ░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█")
+    sleep(10)
+    quit()
+
+def oh_god_why():
+                print("""
+─▄▀▀▀▀▄─█──█────▄▀▀█─▄▀▀▀▀▄─█▀▀▄
+─█────█─█──█────█────█────█─█──█
+─█────█─█▀▀█────█─▄▄─█────█─█──█
+─▀▄▄▄▄▀─█──█────▀▄▄█─▀▄▄▄▄▀─█▄▄▀
+─────────▄██████▀▀▀▀▀▀▄
+─────▄█████████▄───────▀▀▄▄
+──▄█████████████───────────▀▀▄
+▄██████████████─▄▀───▀▄─▀▄▄▄──▀▄
+███████████████──▄▀─▀▄▄▄▄▄▄────█
+█████████████████▀█──▄█▄▄▄──────█
+███████████──█▀█──▀▄─█─█─█───────█
+████████████████───▀█─▀██▄▄──────█
+█████████████████──▄─▀█▄─────▄───█
+█████████████████▀███▀▀─▀▄────█──█
+████████████████──────────█──▄▀──█
+████████████████▄▀▀▀▀▀▀▄──█──────█
+████████████████▀▀▀▀▀▀▀▄──█──────█
+▀████████████████▀▀▀▀▀▀──────────█
+──███████████████▀▀─────█──────▄▀
+──▀█████████████────────█────▄▀
+────▀████████████▄───▄▄█▀─▄█▀
+──────▀████████████▀▀▀──▄███
+──────████████████████████─█
+─────████████████████████──█
+────████████████████████───█
+────██████████████████─────█
+────██████████████████─────█
+────██████████████████─────█
+────██████████████████─────█
+────██████████████████▄▄▄▄▄█
+
+─────────────█─────█─█──█─█───█
+─────────────█─────█─█──█─▀█─█▀
+─────────────█─▄█▄─█─█▀▀█──▀█▀
+─────────────██▀─▀██─█──█───█""")
+sleep(5)
+
+def game_over():
+        sleep(5)
+        print(" ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗") 
+        sleep(0.5)
+        print("██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗")
+        sleep(0.5)
+        print("██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝")
+        sleep(0.5)
+        print("██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗ ") 
+        sleep(0.5)
+        print("╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║")   
+        sleep(0.5)
+        print(" ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝")
+        sleep(10)
+        quit()
+
+def game_over2():
+        sleep(5)
+        print(" ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ ██████╗") 
+        sleep(0.5)
+        print("██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗╚════██╗")
+        sleep(0.5)
+        print("██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝  ▄███╔╝")
+        sleep(0.5)
+        print("██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗  ▀▀══╝") 
+        sleep(0.5)
+        print("╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║  ██╗")   
+        sleep(0.5)
+        print(" ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝  ╚═╝")
+
+def room1_left():
+    global health
+    global pride
+    sleep(2)
+    print("You stumble out of bed")
+    sleep(2)
+    print("A feeling of guilt comes over you as you know you have stood on something exspesive")
+    sleep(5)
+    print("You find and turn on the light switch")
+    sleep(3)
+    print("You see your smashed laptop on the floor")
+    sleep(4)
+    print("The guilt fills your face")
+    sleep(3)
+    oh_god_why()
+    sleep(5)
+    print("You relise that this is your room, but not the way you left it as you closed your eyes last night")
+    sleep(5)
+    print("You are more than confused")
+    sleep(3)
+    print("You go to investigate to see if this irregularity has purged the rest of the house")
+    pride = pride - 10
+
+
+    room2()
+
+def statusbar():
+    print("---------------------------------------------------------")
+    print("Your health is at", health, "and you have", pride, "pride")
+    print("---------------------------------------------------------")
+
 
 def room1():
+    global health
+    global pride
+    print("""
+ _                 _ _             
+| | ___   __ _  __| (_)_ __   __ _ 
+| |/ _ \ / _` |/ _` | | '_ \ / _` |
+| | (_) | (_| | (_| | | | | | (_| |
+|_|\___/ \__,_|\__,_|_|_| |_|\__, |
+                             |___/ """)
 
-    ... make a game
+    sleep(2)
+    print("""
 
 
 
+""")
+    print("-------------")
+    sleep(2)
+    print("Your Bedroom")
+    sleep(1)
+    print("-------------")
+    sleep(1)
+    print("You wake up in the middle of the night in a cold sweat")
+    sleep(3)
+    print("You try to find the light switch, but you do not reconise the layout of the room, but you definitely know the light switch is left")
+    sleep(3)
+    answer = input("Do you go left or right?")
+    if answer.lower() == "right":
+        health = health -9100
+        print("Your health is at", health)
+        print("EGO SUMME DOLORES MAGNA!!!")
+        trollface()
+
+    elif answer.lower() == "left":
+        room1_left()
+
+    elif answer.lower() == "up up down down left right b a start": #Easter egg
+        sleep(5)
+        print("You cheat your way to the end")
+        game_over()
+
+    elif answer.lower() == "go back to sleep":
+        sleep(5)
+        print("You go back to sleep")
+        sleep(2)
+        print("You wake up and everything is how it was")
+        game_over()
+
+    elif answer.lower() == "error":
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        print("error")
+        sleep(0.5)
+        answer = input("Do you go left or right?")
+        if answer.lower() == "right":
+            trollface()
+
+    elif answer.lower() == "left":
+        room1_left()
+
+    else:
+        pride = pride - 9000
+        sleep(5)
+        print("Thinking...")
+        sleep(3)
+        print("You don't know what to do")
+        sleep(1.5)
+        print("You sit idle for 3 days, then collaspe due to cardiac arrest, hunger and thirst")
+        sleep(5)
+        statusbar()
+        sleep(10)
+        quit()
+
+def room2():
+    global health
+    global pride
+    sleep(2)
+    print("-------------")
+    sleep(1)
+    print("The Room")
+    sleep(1)
+    print("-------------")
+    sleep(2)
+    statusbar()
+    sleep(3)
+    print("You open your bedroom door, it takes you to a room you've never seen before")
+    sleep(10)
+    print("In the room everything is upside down")
+    sleep(5)
+    print("The room has it's own gravity, everything falls upwards")
+    sleep(2)
+    print("Everything but you")
+    sleep(3)
+    print("What do you do?")
+    sleep(2)
+    print("Jump through the window or jump down and go through the door")
+    sleep(3)
+    answer = input("door or window")
+    if answer.lower() == "window":
+        sleep(1)
+        print("You jump through the window")
+        sleep(1.5)
+        print("You freefall, and just before you hit the ground you find yourself weightless in the vacuum of space")
+        sleep(3)
+        print("Before you die due to the vacuum of space, you start to go flying backwards")
+        sleep(2)
+        print("You fly through the window you just jumpped out of, and then you smash through the door you decided not to go through")
+        sleep(0.5)
+        print(" ░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄")
+        sleep(0.5)
+        print(" ░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄")
+        sleep(0.5)
+        print(" ░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█")
+        sleep(0.5)
+        print(" ░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░█")
+        sleep(0.5)
+        print(" ░▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░█")
+        sleep(0.5)
+        print(" █▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒█")
+        sleep(0.5)
+        print(" █▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█")
+        sleep(0.5)
+        print(" ░█▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█")
+        sleep(0.5)
+        print(" ░░█░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█")
+        sleep(0.5)
+        print(" ░░░█░░██░░▀█▄▄▄█▄▄█▄████░█")
+        sleep(0.5)
+        print(" ░░░░█░░░▀▀▄░█░░░█░███████░█")
+        sleep(0.5)
+        print(" ░░░░░▀▄░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█")
+        sleep(0.5)
+        print(" ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█")
+        sleep(0.5)
+        print(" ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█")
+        sleep(0.5)
+        print(" ░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█")
+        sleep(10)
+        pride = pride - 20
+        room3()
+
+    elif answer.lower() == "door":
+        print("You jump down, and jump back up and open the next door")
+        sleep(4)
+        print("A blinding light light seeps through the door")
+        sleep(4)
+        print("As you walk through the next door you trip over the door frame, falling into the next adventure")
+        room3()
+
+    elif answer.lower() == "go back in your bedroom":
+        print("You don't feel like going on adventure today")
+        sleep(3)
+        print("You go back to bed and spend the rest of your life in a world you do not belong in")
+        pride = pride + 10
+        game_over2()
+
+
+    elif answer.lower == "parkour out the window":
+        print("You parkour out the window and climb on the roof")
+        sleep(3)
+        print("You jump off the roof for the lols")
+        sleep(2)
+        print("You find yourself in solid gold house")
+        room4()
+
+    else:
+        pride = pride - 9000
+        sleep(5)
+        print("Thinking...")
+        sleep(3)
+        print("You don't know what to do")
+        sleep(1.5)
+        print("You stand idle for 3 days, then collaspe due to cardiac arrest, hunger and thirst")
+        sleep(5)
+        statusbar()
+        sleep(10)
+        quit()
+
+def room3():
+    global health
+    global pride
+    print("----------------")
+    sleep(2)
+    print("Adventure's End")
+    sleep(2)
+    print("----------------")
+    sleep(4)
+    statusbar()
+    print("You find yourself underwater, a blinding light comes from the surface of the floor")
+    sleep(5)
+    print("You are also and equal distance to the top of the water")
+    answer = input("top or bottom")
+    if answer.lower() == "top":
+        print("You swim to the top")
+        sleep(2)
+        print("You swim to shore and you find yourself in the same boring life you were in last night")
+        sleep(5)
+        print("You can't help but wounder what would have happen if you went the other way")
+        game_over2()
+
+    elif answer.lower() == "bottom":
+        pride = -100
+        print("You swim to the bottom")
+        sleep(2)
+        print("You swim to shore and you find yourself in a world ravaged by war and nuclear winter")
+        sleep(5)
+        print("You see a news paper called 'Politics Today' the headline, 'Hillary elected!'")
+        sleep(5)
+        print("You can't help but wounder what would have happen if you went the other way")
+        sleep(2)
+        statusbar()
+        sleep(5)
+        print(" ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗") 
+        sleep(0.5)
+        print("██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗")
+        sleep(0.5)
+        print("██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝")
+        sleep(0.5)
+        print("██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗ ") 
+        sleep(0.5)
+        print("╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║")   
+        sleep(0.5)
+        print(" ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝")
+        sleep(5)
+        print("""Easter eggs (do these in the first room)
+give win = up up down down left right b a start
+error = error
+sleep = go back to sleep
+
+(do these in  room two
+new ending = go back in your bedroom
+new room = parkour out the window""")
+        sleep(2)
+        print("Game will quit in 60 seconds")
+        sleep(60)
+        quit()
+
+
+    else:
+        pride = pride - 9000
+        sleep(5)
+        print("Thinking...")
+        sleep(3)
+        print("You don't know what to do")
+        sleep(1.5)
+        print("You stay idle, then drown")
+        sleep(5)
+        statusbar()
+        sleep(10)
+        quit()
+
+def room4():
+    global health
+    global pride
+    pride = pride + 9000
+    print("--------------------")
+    sleep(2)
+    print("We're cheatin' now")
+    sleep(2)
+    print("--------------------")
+    sleep(2)
+    print("You are in a solid gold house, everyone in the world worships you")
+    sleep(2)
+    print("Everyone in the world also ages very quickly")
+    sleep(2)
+    print("You are unable to connect or make friends with anyone")
+    sleep(3)
+    print("You spend the rest of your life surrounded by people you don't like")
+    sleep(3)
+    print("You die at the ripe old age of 86")
+    print("You can't help but wounder what would have happen if you went the other way")
+    sleep(4)
+    statusbar()
+    sleep(5)
+    print(" ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗") 
+    sleep(0.5)
+    print("██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗")
+    sleep(0.5)
+    print("██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝")
+    sleep(0.5)
+    print("██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗ ") 
+    sleep(0.5)
+    print("╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║")   
+    sleep(0.5)
+    print(" ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝")
+    sleep(5)
+    print("""Easter eggs (do these in the first room)
+give win = up up down down left right b a start
+error = error
+sleep = go back to sleep
+
+(do these in  room two
+new ending = go back the in your bedroom
+new room = parkour out the window""")
+    sleep(2)
+    print("Game will quit in 60 seconds")
+    sleep(60)
+    quit()
+            
+        
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,8 +1,67 @@
 # Our quiz!
 
-def quiz():
+score = 0
+name = input("Oi, wots ya name?")
 
-    ... make a quiz!
+def quiz():
+    global score
+    global name
+    print("Salutations", name)
+    print("Get reedy m8, cos we r gonna se if ya smarter than an Isaac")
+    print("Question 1, my dear Sir",name,"Is Bob's change possible?")
+    print("A. Nah m8 me and all me roadmen in the world can't de it")
+    print("B. Yes my dear Sir",name,"it is quite possible when the room is at optimal temprature, I say quizzaciously")
+    print("C. When everyone is taking turns speaking telling me the answer it is quite hard to concentrate")
+    answer = input("Think, what would an Isaac do, and DO NOT click that answer!")
+    if answer.lower() == "a":
+        print("Tell me bout it bruv, I denit knee wot im doin wit it blood")
+        print("This answer is neither more nor less than an Isaac, one point!")
+        score = 1
+
+    elif answer.lower() == "b":
+        print("Why, yes my dear Sir",name)
+        print("I can see we are dealing with a fellow man of intellect")
+        print("This answer is by far and away much more intelligent than any Isaac would answer. One thousand points to you my man")
+        score = 1000
+
+    elif answer.lower() == "c":
+        print("You're an Isaac, aren't you? If you are an Isaac zero points, if you are not an Isaac negative one thousand points, I'll give you zero points, but if your name isn't Isaac I'll trust you'll be honest")
+        score = -1000
+        print("jk")
+
+    else:
+        print("Are you trying to break my game? My game I worked hard on so people can have fun? Programers have feelings too, -99999999999999999999999999999999 points")
+        score = -99999999999999999999999999999999
+
+
+    print("Question 2 my dear Sir",name,"What is Isaac's last name?")
+    print("A. It's Matthews isn't it?")
+    print("B. Douglas, it says on his birth certificate")
+    print("C. Mate, it's Watson, thats his new-est Dad's name, it has to be Watson")
+    print("D. Who is this Isaac we are talking about?")
+    answer = input("Think, what does Isaac say his name is and what is his legal name?")
+    if answer.lower() == "a":
+        print("Many people think this but it is nothing more than an old wives tale")
+        print("Zero points")
+
+    elif answer.lower() == "b":
+        print("Too be honests, I don't know")
+        print("1 point?")
+        score = 1
+
+    elif answer.lower() == "c":
+        print("He says it isn't but between me and you, it is.")
+        print("10 points")
+        score = 10
+
+    elif answer.lower() == "d":
+        print("Oh, how I wish I was you")
+        print("One thousand points")
+        score = 1000
+
+    else:
+        print("Are you trying to break my game? My game I worked hard on so people can have fun? Programers have feelings too, -99999999999999999999999999999999 points")
+        score = -99999999999999999999999999999999
 
 
 
